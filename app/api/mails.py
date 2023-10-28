73a6a8568187a3e3d724f6ad5ea63c5ed9ebf97b
Abhinav <66725911+Abhinav16072001@users.noreply.github.com>
2023-10-28 22:47:42 +0530

    downloads api

--- app/api/mails.py
+++ app/api/mails.py
@@ -1,4 +1,5 @@
 import yaml
+import datetime
 from fastapi import APIRouter, HTTPException, Depends, WebSocket
 from sqlalchemy.orm import Session
 from app.database.session import get_db
@@ -22,20 +23,48 @@ def mails_info(days: int):
     info = fetch_email_info(username, app_password, days)
     if info is None:
         raise HTTPException(status_code=404, detail="info not found")
-    file_name = convert_to_excel("Email_info",info)
-    return file_name
+    return info
 
 @router.get("/count/{days}")
 def read_mails(days: int):
-    mails = count_emails_received(username, app_password, days)
-    if mails is None:
+    mails_count = count_emails_received(username, app_password, days)
+    if mails_count is None:
         raise HTTPException(status_code=404, detail="count not found")
-    return mails
+    return mails_count
 
 @router.get("/senders/{days}")
 def read_mails(days: int):
     senders = unique_senders_count(username, app_password, days)
     if senders is None:
         raise HTTPException(status_code=404, detail="senders not found")
-    file_name = convert_to_excel("Unique_senders_count_info", senders)
     return senders
+
+@router.get("/senders/{days}/download")
+def download_senders(days: int):
+    senders = unique_senders_count(username, app_password, days)
+    if senders is None:
+        raise HTTPException(status_code=404, detail="Senders not found")
+    current_date = datetime.now()
+    date = current_date.strftime('%c').split()[:3]
+    filename = f"senders_{'_'.join(date)}"
+
+    convert_to_csv(filename, senders)
+    response = JSONResponse(content=senders)
+    response.headers["Content-Disposition"] = f'attachment; filename="{filename}.csv"'
+
+    return response
+
+@router.get("/info/{days}/download")
+def download_count(days: int):
+    info = fetch_email_info(username, app_password, days)
+    if info is None:
+        raise HTTPException(status_code=404, detail="info not found")
+    current_date = datetime.now()
+    date = current_date.strftime('%c').split()[:3]
+    filename = f"email_info_{'_'.join(date)}"
+
+    convert_to_csv(filename, info)
+    response = JSONResponse(content=info)
+    response.headers["Content-Disposition"] = f'attachment; filename={filename}.csv"'
+    
+    return response
--- app/connections/convert.py
+++ app/connections/convert.py
@@ -1,33 +1,13 @@
 import os
 import pandas as pd
-from openpyxl import Workbook
-from openpyxl.styles import Font, Alignment, NamedStyle
-from openpyxl.utils.dataframe import dataframe_to_rows
 
-def convert_to_excel(filename, data_list):
-    output_dir = os.getcwd()
-    file_name = os.path.join(output_dir, f"{filename}.xlsx")
+def convert_to_csv(filename, data_list):
+    download_path = os.path.join(os.path.expanduser("~"), "Downloads")
+    file_name = os.path.join(download_path, f"{filename}.csv")
 
     df = pd.DataFrame(data_list)
 
-    writer = pd.ExcelWriter(file_name, engine='openpyxl')
-
-    df.to_excel(writer, sheet_name='Sheet1', index=False)
-
-    workbook = writer.book
-    worksheet = writer.sheets['Sheet1']
-
-    date_style = NamedStyle(name='date_style')
-    date_style.font = Font(bold=True)
-    date_style.alignment = Alignment(horizontal='left', vertical='center')
-
-    column_widths = [max(len(str(val)) for val in df[col]) + 2 for col in df.columns]
-    for i, width in enumerate(column_widths, 1):
-        worksheet.column_dimensions[worksheet.cell(row=1, column=i).column_letter].width = width
-
-    for cell in worksheet['1']:
-        cell.style = date_style
-
-    workbook.save(file_name)
+    # Save the DataFrame to a CSV file
+    df.to_csv(file_name, index=False)
 
     return file_name
